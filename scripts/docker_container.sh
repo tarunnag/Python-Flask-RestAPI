5e900e56455c5353f59d5d3aa03a2cfe41aa7b18
tarunnagb <tarunnagb@gmail.com>
2024-09-16 19:19:16 +0530

    14th_commit

--- scripts/docker_container.sh
+++ scripts/docker_container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-/usr/local/bin/docker-compose down
-/usr/local/bin/docker-compose up -d
+docker-compose down
+docker-compose up -d
 sleep 60
 
 
-/usr/local/bin/docker-compose -f docker-compose1.yml down
-/usr/local/bin/docker-compose -f docker-compose1.yml up -d
+docker-compose -f docker-compose1.yml down
+docker-compose -f docker-compose1.yml up -d
 sleep 60
 
 

f6f08d117bd479e5e44c9236efa393e306c840fb
tarunnagb <tarunnagb@gmail.com>
2024-09-16 19:48:17 +0530

    16th_commit

--- scripts/docker_container.sh
+++ scripts/docker_container.sh
@@ -12,13 +12,13 @@ set -e
 
 docker pull 585768149561.dkr.ecr.ap-south-1.amazonaws.com/python-image:latest
 
-docker stop python-container || true
-docker rm python-conatiner || true
+#docker stop python-container || true
+#docker rm python-conatiner || true
 
 docker run -d -p 5001:5000 --name python-container 585768149561.dkr.ecr.ap-south-1.amazonaws.com/python-image:latest
 
-docker stop python-container1 || true
-docker rm python-container1 || true
+#docker stop python-container1 || true
+#docker rm python-container1 || true
 
 docker run -d -p 5000:5000 --name python-container1 585768149561.dkr.ecr.ap-south-1.amazonaws.com/python-image:latest
 

698b076a34467f81a4e54eebc149439001557de9
tarunnagb <tarunnagb@gmail.com>
2024-09-16 19:46:00 +0530

    15th_commit

--- scripts/docker_container.sh
+++ scripts/docker_container.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 set -e
 
-docker-compose down
-docker-compose up -d
-sleep 60
+#docker-compose down
+#docker-compose up -d
+#sleep 60
 
 
-docker-compose -f docker-compose1.yml down
-docker-compose -f docker-compose1.yml up -d
-sleep 60
+#docker-compose -f docker-compose1.yml down
+#docker-compose -f docker-compose1.yml up -d
+#sleep 60
+
+docker pull 585768149561.dkr.ecr.ap-south-1.amazonaws.com/python-image:latest
+
+docker stop python-container || true
+docker rm python-conatiner || true
+
+docker run -d -p 5001:5000 --name python-container 585768149561.dkr.ecr.ap-south-1.amazonaws.com/python-image:latest
+
+docker stop python-container1 || true
+docker rm python-container1 || true
+
+docker run -d -p 5000:5000 --name python-container1 585768149561.dkr.ecr.ap-south-1.amazonaws.com/python-image:latest
 
 
 
